ea47280444ec90ece157e5c73cd4a7c8254ffdc6
nurkholis <nur.kholis@ist.id>
2023-01-22 21:29:41 +0700

    Initial Commit

---
+++ Rick&Morty-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty-App
+//
+//  Created by nur kholis on 22/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
